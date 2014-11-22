bc7ddceb23a9d8ad3c8c1935fb2a2ecf89b5de45
Rav Mahov <ravmahov@gmail.com>
2014-11-22 10:01:22 +0100

    added: multiple.h added: add.h fixed: main.cpp (lacked () near main) added: .gitignore

--- main.cpp
+++ main.cpp
@@ -1,3 +1,8 @@
-int main{
+#include <iostream>
+#include "multiple.h"
+#include "add.h"
+
+int main(){
+	std::cout << multiple(2, add(2, 4)) << std::endl;
 	return 0;
 }
---
+++ multiple.h
@@ -0,0 +1,9 @@
+#ifndef MULTIPLE_H
+#define MULTIPLE_H
+
+int multiple(int a, int b)
+{
+	return a * b;
+}
+
+#endif
